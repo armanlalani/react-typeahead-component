58e15f30d8a3ee0276fcf892a89e49ba4ec4261d
Arman <armanlalani5@gmail.com>
2025-03-17 18:25:28 +0530

    Handling race condition

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -64,17 +64,30 @@ const fruits = [
   "Tangerine",
   "Watermelon",
 ];
+let idx = 0;
+const simulateRaceCondition = true;
 
-const getFruits = (query) => {
+const getFruits = (query, { signal }) => {
   console.log("Called", query);
   return new Promise((res, rej) => {
     const filteredFruits = fruits.filter((fruit) =>
       fruit.toLowerCase().includes(query.toLowerCase())
     );
 
+    const delay = simulateRaceCondition
+      ? idx === 0
+        ? 3000
+        : 300
+      : Math.random() * 1000;
+
     setTimeout(() => {
-      res(filteredFruits);
-    }, Math.random() * 1000);
+      if (signal.aborted) {
+        rej(new Error("Abort Error"));
+      } else {
+        res(filteredFruits);
+      }
+    }, delay);
+    idx++;
   });
 };
 
@@ -104,23 +117,60 @@ function Autocomplete() {
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const debouncedQuery = useDebounce(query, 300);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
 
   const handlQueryChange = (e) => {
     setQuery(e.target.value);
   };
 
-  const getData = useCallback(async () => {
-    const data = await getFruits(debouncedQuery);
-    setSuggestions(data);
-  }, [debouncedQuery]);
+  const getData = useCallback(async (debouncedQuery, signal) => {
+    try {
+      setIsLoading(true);
+      setSuggestions([]);
+
+      const data = await getFruits(debouncedQuery, {
+        signal,
+      });
+      setSuggestions(data);
+    } catch (err) {
+      if (err.message === "Abort Error") return;
+      console.dir(err);
+      setIsError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
+    const controller = new AbortController();
     if (debouncedQuery.trim().length > 3) {
-      getData();
+      getData(debouncedQuery, controller.signal);
     } else {
       setSuggestions([]);
     }
+
+    // The cleanup function is important to avoid race condition
+    return () => controller.abort();
   }, [debouncedQuery, getData]);
+
+  /** START: Alternative to useDebounce */
+  //   const getData = async () => {
+  //     const data = await getFruits(query);
+  //     setSuggestions(data);
+  //   };
+
+  //   useEffect(() => {
+  //     let token = null;
+  //     if (query.trim().length > 3) {
+  //       token = setTimeout(getData, 300);
+  //     } else {
+  //       setSuggestions([]);
+  //     }
+
+  //     return () => clearTimeout(token);
+  //   }, [query]);
+  /** END */
 
   return (
     <div className="autocomplete-container">
@@ -130,8 +180,9 @@ function Autocomplete() {
         placeholder="Search..."
         onChange={handlQueryChange}
       />
+      {isLoading && <p className="suggestion-container">Loading...</p>}
       {suggestions.length > 0 && (
-        <ul className="suggestion-box">
+        <ul className="suggestion-container suggestion-box">
           {suggestions.map((item) => (
             <li key={item} className="suggestion-item">
               {item}
